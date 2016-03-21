7afbc54b3752a041062041d94d33ef7d3e7e516d
justinthestickman <JCHuang@student.naperville203.org>
2016-03-21 15:10:17 -0500

    Started WC 3/21/16

---
+++ Chapter 11 Practice/WC.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+import java.io.*;
+
+
+public class WC
+{
+    public static void main(String[] args)
+    {
+        try
+        {
+            Scanner temp = new Scanner(System.in);
+            System.out.println("File Name: ");
+            String fileName = temp.next();
+            
+            File inputFile = new File(fileName);
+            Scanner in = new Scanner(inputFile);
+            
+            in.useDelimiter("");
+            int counter = 0;
+            while (in.hasNext())
+            {
+                counter += 1;
+            }
+            System.out.println("Number of Characters: " + counter);
+            
+            in.useDelimiter(" ");
+            counter = 0;
+            while (in.hasNext())
+            {
+                counter += 1;
+            }
+            System.out.println("Number of Words: " + counter);
+            
+            
+            
+            in.close();
+        }
+        catch (Exception e)
+        {
+            
+        }
+    }
+}
