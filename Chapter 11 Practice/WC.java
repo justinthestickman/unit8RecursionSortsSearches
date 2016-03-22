44a7745d0d048e6dd6554ad40157b6265aa693d9
justinthestickman <JCHuang@student.naperville203.org>
2016-03-22 15:04:39 -0500

    I have no idea how to do any of this...textbook time

--- Chapter 11 Practice/WC.java
+++ Chapter 11 Practice/WC.java
@@ -8,11 +8,11 @@ public class WC
     {
         try
         {
-            Scanner temp = new Scanner(System.in);
-            System.out.println("File Name: ");
-            String fileName = temp.next();
-            
-            File inputFile = new File(fileName);
+            //Scanner temp = new Scanner(System.in);
+            //System.out.print("File Name: ");
+            //String fileName = temp.next();
+            File inputFile = new File("biblekjv.txt");
+            //File inputFile = new File(fileName);
             Scanner in = new Scanner(inputFile);
             
             in.useDelimiter("");
@@ -31,11 +31,17 @@ public class WC
             }
             System.out.println("Number of Words: " + counter);
             
-            
+            in.useDelimiter("\n");
+            counter = 0;
+            while (in.hasNext())
+            {
+                counter += 1;
+            }
+            System.out.println("Number of Lines: " + counter);
             
             in.close();
         }
-        catch (Exception e)
+        catch (IOException ioe)
         {
             
         }
