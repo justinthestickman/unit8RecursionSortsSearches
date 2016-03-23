d65836c4560f282bf97b4e703127ff5544aa045c
justinthestickman <JCHuang@student.naperville203.org>
2016-03-23 15:09:10 -0500

    Finished Word Count 3/23/16

--- Chapter 11 Practice/WC.java
+++ Chapter 11 Practice/WC.java
@@ -1,49 +1,52 @@
 import java.util.Scanner;
 import java.io.*;
 
-
 public class WC
 {
-    public static void main(String[] args)
+    public static void main(String[] args) throws IOException
     {
         try
         {
-            //Scanner temp = new Scanner(System.in);
-            //System.out.print("File Name: ");
-            //String fileName = temp.next();
-            File inputFile = new File("biblekjv.txt");
-            //File inputFile = new File(fileName);
-            Scanner in = new Scanner(inputFile);
+            Scanner console = new Scanner(System.in);
+            System.out.print("File Name: ");
+            String fileName = console.next();
+            File file = new File(fileName);
             
-            in.useDelimiter("");
+            Scanner in = new Scanner(file);
             int counter = 0;
+            in.useDelimiter("");
             while (in.hasNext())
             {
+                String character = in.next();
                 counter += 1;
             }
             System.out.println("Number of Characters: " + counter);
+            in.close();
             
-            in.useDelimiter(" ");
+            in = new Scanner(file);
             counter = 0;
             while (in.hasNext())
             {
+                String word = in.next();
                 counter += 1;
             }
             System.out.println("Number of Words: " + counter);
+            in.close();
             
-            in.useDelimiter("\n");
+            in = new Scanner(file);
             counter = 0;
+            in.useDelimiter("\n");
             while (in.hasNext())
             {
+                String line = in.next();
                 counter += 1;
             }
             System.out.println("Number of Lines: " + counter);
-            
             in.close();
         }
         catch (IOException ioe)
         {
-            
+            System.out.println("File does not exist");
         }
     }
 }
